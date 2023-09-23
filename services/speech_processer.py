bf85eb1020809a301b48b5bfc3900228e0d41b40
Portnov Artem <artemprod1@gmail.com>
2023-09-23 15:57:51 +0400

    Добавлен интерфейс + git ignore + speach processer

--- services/speech_processer.py
+++ services/speech_processer.py
@@ -5,10 +5,18 @@ import json
 model = whisper.load_model('base')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def speech_to_voice(file_path: str) -> str:
+def speech_to_voice_with_path(file_path: str) -> str:
     try:
         audio= whisper.load_audio(file_path)
         transcibed_result = model.transcribe(audio)
+        return transcibed_result['text']
+    except:
+        print('could not transcibe audio')
+        return False
+
+def speech_to_voice(file) -> str:
+    try:
+        transcibed_result = model.transcribe(file)
         return transcibed_result['text']
     except:
         print('could not transcibe audio')
