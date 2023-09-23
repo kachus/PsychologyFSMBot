ffe5335a5bca6d645ee003884751bd2f60637b6f
kachus <ka4usova.e@gmail.com>
2023-09-23 14:09:21 +0400

    added new func speech processor

--- services/speech_processer.py
+++ services/speech_processer.py
@@ -5,7 +5,7 @@ import json
 model = whisper.load_model('base')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def speech_to_voice(file_path: str) -> str:
+def speech_to_voice_with_path(file_path: str) -> str:
     try:
         audio= whisper.load_audio(file_path)
         transcibed_result = model.transcribe(audio)
@@ -13,4 +13,16 @@ def speech_to_voice(file_path: str) -> str:
     except:
         print('could not transcibe audio')
         return False
+
+def speech_to_voice(file) -> str:
+    try:
+        transcibed_result = model.transcribe(file)
+        return transcibed_result['text']
+    except:
+        print('could not transcibe audio')
+        return False
+
+
+
+
 
